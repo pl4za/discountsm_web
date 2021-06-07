d1e068b36db2c998e9192a957ab7f6660b51b287
Jason Costa <jason.costa@starlingbank.com>
2021-06-07 22:59:03 +0100

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 // @flow
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import Button from 'react-bootstrap/Button';
+import { Container, Row, Col, Nav, Image, ListGroup } from 'react-bootstrap';
 
 import "bootswatch/dist/superhero/bootstrap.min.css"
 import './App.css';
@@ -10,53 +9,38 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Container fluid="sd">
+      <Row>
+        <Col xs={1}><Image src={logo} rounded /></Col>
+        <Col className="d-flex align-items-center">
+        <Nav defaultActiveKey="/home" as="ul">
+          <Nav.Item as="li">
+            <Nav.Link href="/home">All</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <ListGroup>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Nav className="justify-content-center pagination" as="ul">
+          <Nav.Item className="page-item disabled" as="li">
+            <Nav.Link className="page-link" href="#">&laquo;</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="page-item active" as="li">
+            <Nav.Link className="page-link" href="#">1</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="page-item" as="li">
+            <Nav.Link className="page-link" href="#">&raquo;</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Row>
+    </Container>
   );
 }
 
