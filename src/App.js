290789eb1156f0e5b7b10d26c2c453c615b9d97d
Jason Costa <jason.costa@starlingbank.com>
2021-06-09 17:02:41 +0100

    Tweak deal card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import logo from './logo.svg';
-import { Container, Row, Col, Nav, Image, ButtonGroup, Button, Card } from 'react-bootstrap';
+import { Container, Row, Col, Nav, Image, ButtonGroup, Button, Card, Figure } from 'react-bootstrap';
 import { ArrowUp, ArrowDown, Basket, CalendarDate } from 'react-bootstrap-icons';
 
 import './App.scss';
@@ -9,6 +9,7 @@ import './App.scss';
 function App() {
   return (
     <Container fluid>
+
       {/* header */}
       <Row>
         <Col><Image src={logo} rounded /></Col>
@@ -20,51 +21,57 @@ function App() {
           </Nav>
         </Col>
       </Row>
-      {/* deal item 1 */}
-      <Container fluid>
-        <Row>
-          <Card>
-            <Card.Body>
-              <Container className="zero-padding">
-                <Row className="align-items-center justify-content-between">
-                  <Col><Image src={logo} /></Col>
-                  <Col xs={8}>
-                    <Card.Title>Bose® QuietComfort Headphones in Black</Card.Title>
-                    <Card.Text>
-                      <Container>
-                        <Row className="align-items-center">
-                          <h3 className="zero-bottom-margin">£11.50</h3> | Boots Deals
+
+      {/* deal item */}
+      <Card>
+        <Card.Body as="deal-card-body">
+          <Container as="flat-container">
+            <Row as="deal-row">
+              <Col>
+                <Figure>
+                  <Figure.Image
+                    width={180}
+                    height={180}
+                    alt="deal_image"
+                    src={logo} />
+                </Figure>
+              </Col>
+              <Col xs={8}>
+                <Card.Title>Bose® QuietComfort Headphones in Black</Card.Title>
+                <Card.Text>
+                  <Container>
+                    <Row className="align-items-center">
+                      <h3 className="zero-bottom-margin">£11.50</h3>| Boots Deals
                         </Row>
-                        <Row>
-                          Decent offer. Features - Noise Cancellation - Up to 20 Hours PlaytimeDecent offer. Features - Noise Cancellation - Up to 20 Hours Playtime (Featuring Quick Charge) - Soft Alcantara Fabric Headband - Noise-Rejecting Dual Microphone - Volume
-                        </Row>
-                      </Container>
-                    </Card.Text>
+                    <Row className="crop-text">
+                      Decent offer. Features - Noise Cancellation - Up to 20 Hours Playtime (Featuring Quick Charge) - Soft Alcantara Fabric Headband - Noise-Rejecting Dual Microphone - Volume
+                    </Row>
+                  </Container>
+                </Card.Text>
+              </Col>
+              <Col md="auto">
+                <ButtonGroup vertical>
+                  <Button><ArrowUp /></Button>
+                  <Button><ArrowDown /></Button>
+                </ButtonGroup>
+              </Col>
+            </Row>
+          </Container>
+        </Card.Body>
+        <Card.Footer as="card-footer">
+          <Container as="card-footer-container">
+            <Row as="container-footer-row">
+              <Col md="auto">
+                <CalendarDate /> 2 days ago by JasonCosta
                   </Col>
-                  <Col md="auto">
-                    <ButtonGroup vertical>
-                      <Button><ArrowUp /></Button>
-                      <Button><ArrowDown /></Button>
-                    </ButtonGroup>
-                  </Col>
-                </Row>
-              </Container>
-            </Card.Body>
-            <Card.Footer as="slim-card-footer">
-              <Container className="zero-padding">
-                <Row className="align-items-center justify-content-between">
-                  <Col md="auto">
-                    <CalendarDate /> 2 days ago by JasonCosta
-                  </Col>
-                  <Col md="auto">
-                    <Button size="sm"><Basket /> See deal</Button>
-                  </Col>
-                </Row>
-              </Container>
-            </Card.Footer>
-          </Card>
-        </Row>
-      </Container>
+              <Col md="auto">
+                <Button size="sm"><Basket /> See deal</Button>
+              </Col>
+            </Row>
+          </Container>
+        </Card.Footer>
+      </Card>
+
     </Container>
   );
 }
