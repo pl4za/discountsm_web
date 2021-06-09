f91e6a1d9bc5b19c8ef8202980094bc8d3776d2c
Jason Costa <jason.costa@starlingbank.com>
2021-06-09 16:14:01 +0100

    Add deal card

--- src/App.js
+++ src/App.js
@@ -1,45 +1,70 @@
 // @flow
 import React from 'react';
 import logo from './logo.svg';
-import { Container, Row, Col, Nav, Image, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Nav, Image, ButtonGroup, Button, Card } from 'react-bootstrap';
+import { ArrowUp, ArrowDown, Basket, CalendarDate } from 'react-bootstrap-icons';
 
-import "bootswatch/dist/superhero/bootstrap.min.css"
-import './App.css';
 import './App.scss';
 
 function App() {
   return (
-    <Container fluid="sd">
+    <Container fluid>
+      {/* header */}
       <Row>
-        <Col xs={1}><Image src={logo} rounded /></Col>
+        <Col><Image src={logo} rounded /></Col>
         <Col className="d-flex align-items-center">
-        <Nav defaultActiveKey="/home" as="ul">
-          <Nav.Item as="li">
-            <Nav.Link href="/home">All</Nav.Link>
-          </Nav.Item>
-        </Nav>
+          <Nav defaultActiveKey="/home" as="ul">
+            <Nav.Item as="li">
+              <Nav.Link href="/home">All</Nav.Link>
+            </Nav.Item>
+          </Nav>
         </Col>
       </Row>
-      <Row>
-        <Col>
-          <ListGroup>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          </ListGroup>
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
-        <Nav className="justify-content-center pagination" as="ul">
-          <Nav.Item className="page-item disabled" as="li">
-            <Nav.Link className="page-link" href="#">&laquo;</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="page-item active" as="li">
-            <Nav.Link className="page-link" href="#">1</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="page-item" as="li">
-            <Nav.Link className="page-link" href="#">&raquo;</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Row>
+      {/* deal item 1 */}
+      <Container fluid>
+        <Row>
+          <Card>
+            <Card.Body>
+              <Container className="zero-padding">
+                <Row className="align-items-center justify-content-between">
+                  <Col><Image src={logo} /></Col>
+                  <Col xs={8}>
+                    <Card.Title>Bose® QuietComfort Headphones in Black</Card.Title>
+                    <Card.Text>
+                      <Container>
+                        <Row className="align-items-center">
+                          <h3 className="zero-bottom-margin">£11.50</h3> | Boots Deals
+                        </Row>
+                        <Row>
+                          Decent offer. Features - Noise Cancellation - Up to 20 Hours PlaytimeDecent offer. Features - Noise Cancellation - Up to 20 Hours Playtime (Featuring Quick Charge) - Soft Alcantara Fabric Headband - Noise-Rejecting Dual Microphone - Volume
+                        </Row>
+                      </Container>
+                    </Card.Text>
+                  </Col>
+                  <Col md="auto">
+                    <ButtonGroup vertical>
+                      <Button><ArrowUp /></Button>
+                      <Button><ArrowDown /></Button>
+                    </ButtonGroup>
+                  </Col>
+                </Row>
+              </Container>
+            </Card.Body>
+            <Card.Footer as="slim-card-footer">
+              <Container className="zero-padding">
+                <Row className="align-items-center justify-content-between">
+                  <Col md="auto">
+                    <CalendarDate /> 2 days ago by JasonCosta
+                  </Col>
+                  <Col md="auto">
+                    <Button size="sm"><Basket /> See deal</Button>
+                  </Col>
+                </Row>
+              </Container>
+            </Card.Footer>
+          </Card>
+        </Row>
+      </Container>
     </Container>
   );
 }
